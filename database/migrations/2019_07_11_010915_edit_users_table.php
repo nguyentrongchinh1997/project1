51cd321c6b6b1f41498d74917a3c4f2f56e3ae5c
nguyen trong chinh <chinh20091997@gmail.com>
2019-07-23 00:03:33 +0700

    create migration

---
+++ database/migrations/2019_07_11_010915_edit_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->float("balance", 15, 2)->nullable();
+            $table->integer("level")->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['balance', 'level']);
+        });
+    }
+}
